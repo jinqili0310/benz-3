25303c92f3a3ca67a60e0cf3b4108ce659883bd0
Jinqi Li <jxl2047@miami.edu>
2020-12-01 02:12:26 -0500

    Updates

---
+++ precache-manifest.43005dbe75aaf7f561f2b38dd43bc88f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ed6efb674e2b1068a5763ea5eaa4b8",
+    "url": "/benz-3/index.html"
+  },
+  {
+    "revision": "7755565ec5fde32d138a",
+    "url": "/benz-3/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bc86116098c9377459fb",
+    "url": "/benz-3/static/css/main.b0090f60.chunk.css"
+  },
+  {
+    "revision": "7755565ec5fde32d138a",
+    "url": "/benz-3/static/js/2.d118be08.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/benz-3/static/js/2.d118be08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc86116098c9377459fb",
+    "url": "/benz-3/static/js/main.ec73cff3.chunk.js"
+  },
+  {
+    "revision": "d8dacc3b4c6df2ab8c15",
+    "url": "/benz-3/static/js/runtime-main.3bc3b450.js"
+  },
+  {
+    "revision": "93ac4ac6f4d13f87b52bc083b034c9a9",
+    "url": "/benz-3/static/media/19C0840_005_FVP.93ac4ac6.jpg"
+  },
+  {
+    "revision": "2d23408d634eaa9ddf605eee971761f8",
+    "url": "/benz-3/static/media/2020-GLB-SUV-CH-1-1-DR.2d23408d.jpg"
+  },
+  {
+    "revision": "294517669b71c34024f42800cbc4780d",
+    "url": "/benz-3/static/media/2020-GLC-SUV-CH-1-1-DR.29451766.jpg"
+  },
+  {
+    "revision": "30f37537f5714782e7d53fc1906edd59",
+    "url": "/benz-3/static/media/2021-GLA-SUV-CH-1-1-DR.30f37537.jpg"
+  },
+  {
+    "revision": "19c4ec4af9304d4bfe4ec44c28c28543",
+    "url": "/benz-3/static/media/Accessory-Vehicle-XL-865x600.19c4ec4a.jpg"
+  },
+  {
+    "revision": "9d6fbd2c02a7b3c007566ab2a50965f4",
+    "url": "/benz-3/static/media/DaimlerCAC-Regular.9d6fbd2c.ttf"
+  },
+  {
+    "revision": "e63c51684446b7bd1db4b80c8f095e38",
+    "url": "/benz-3/static/media/DaimlerCS-Regular.e63c5168.ttf"
+  },
+  {
+    "revision": "e375ef61eaf371666981c95130b48d9a",
+    "url": "/benz-3/static/media/HKW_SE2_DR.e375ef61.jpg"
+  },
+  {
+    "revision": "8b7855edecc59db488aa5a544c9bbec2",
+    "url": "/benz-3/static/media/MBCorpoATitleCond-Regular-Web.8b7855ed.ttf"
+  },
+  {
+    "revision": "2e84a616f5063915e63a9773777938a9",
+    "url": "/benz-3/static/media/MBCorpoSText-Bold-Web.2e84a616.ttf"
+  },
+  {
+    "revision": "e8fb216e85cc9f877bc87d9772eb15be",
+    "url": "/benz-3/static/media/MBCorpoSText-Regular-Web.e8fb216e.ttf"
+  },
+  {
+    "revision": "1fc399815b8a9d8e85e3ee8964e57cc7",
+    "url": "/benz-3/static/media/Mercedes_Benz__logo--desktop.1fc39981.png"
+  },
+  {
+    "revision": "3313c05350079e551bf669d8c518cd2b",
+    "url": "/benz-3/static/media/accessories_865x600.3313c053.jpg"
+  }
+]);
